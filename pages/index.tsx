a1e758b2a64186bc9f9987811eecf071a894672e
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-07-30 21:01:43 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,9 @@ import Presente from "../components/Presente"
 import { atualizarPortas, criarPortas } from "../functions/portas"
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPortas(){
-    return portas.map(porta=>{
-      return <Porta key={porta.numero} value={porta} 
-      onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-    })
-  }
-
   return (
     <div style={{display:'flex'}}>
-        {renderizarPortas()}
+        <h1>Formulario de Inicio</h1>
     </div>
   )
 }
